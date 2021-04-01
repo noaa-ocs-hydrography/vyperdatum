6ff017ff79f78b1c7c639171c5b416fed346b341
eyou102 <eyou102@gmail.com>
2021-04-01 10:55:11 -0400

    add setup.py

--- vyperdatum/__init__.py
+++ vyperdatum/__init__.py
@@ -1,5 +1,7 @@
 from osgeo import gdal
 
+from vyperdatum.__version__ import __version__
+
 version = gdal.VersionInfo()
 major = int(version[0])
 minor = int(version[1:3])
@@ -8,3 +10,4 @@ if not (major == 3 and minor >= 1):
     msg = f'The version of GDAL must be >= 3.1.\
             Version found: {major}.{minor}.{bug}'
     raise ValueError(msg)
+
